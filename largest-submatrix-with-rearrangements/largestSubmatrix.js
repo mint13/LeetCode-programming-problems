14419defc3711835d5b1168aa8edf36b3148f228
Mint Medillo <m.medillo13@gmail.com>
2021-09-20 20:40:30 +0800

    create counting function

--- largest-submatrix-with-rearrangements/largestSubmatrix.js
+++ largest-submatrix-with-rearrangements/largestSubmatrix.js
@@ -2,3 +2,28 @@
  * @param {number[][]} matrix
  * @return {number}
  */
+
+ const largestSubmatrix = (matrix) => {
+
+}
+
+const counting = (column, max) => {
+	let sorted = [...column].sort( (a, b) => b - a )
+	let k, j
+	for (let i = 0; i < sorted.length; i++) {
+		if (sorted[i] === sorted[i - 1])
+			continue
+		
+		k = j = i
+		
+		while (sorted[j] && sorted[j] >= sorted[i] ) 
+			j++
+		
+		while (sorted[k] && sorted[k] >= sorted[i])
+			k--
+		
+		max = Math.max(sorted[i] * Math.abs(k - j + 1), max)
+	}
+
+	return max
+}
