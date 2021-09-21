4f7e1a33d42deff73ce29e67317d119bb91ea0f9
Mint Medillo <m.medillo13@gmail.com>
2021-09-21 13:47:35 +0800

    create depthFirstSearch function

--- surrounded-region/surroundedRegion.js
+++ surrounded-region/surroundedRegion.js
@@ -2,3 +2,16 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
+const solve = (board) => {
+
+}
+
+const depthFirstSearch = (board, x, y, xLen, yLen) => {
+  if (x >= 0 && y >= 0 && x < xLen && y < yLen && board[x][y] === 'O') {
+    board[x][y] = '-'
+    depthFirstSearch(board, x + 1, y, xLen, yLen)
+    depthFirstSearch(board, x - 1, y, xLen, yLen)
+    depthFirstSearch(board, x, y + 1, xLen, yLen)
+    depthFirstSearch(board, x, y - 1, xLen, yLen)
+  }
+}
