877045395082dbaa5f9bfb9690fbcfd8b64e69c7
Mint Medillo <m.medillo13@gmail.com>
2021-09-21 13:48:21 +0800

    solve function logic

--- surrounded-region/surroundedRegion.js
+++ surrounded-region/surroundedRegion.js
@@ -3,7 +3,25 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 const solve = (board) => {
-
+	let xLen = board.length
+	let yLen = (board[0] || []).length
+	
+	for (let i = 0; i < xLen; i++) {
+		depthFirstSearch(board, i, 0, xLen, yLen)
+		depthFirstSearch(board, i, yLen - 1, xLen, yLen)
+	}
+	for (let j = 0; j < yLen; j++) {
+		depthFirstSearch(board, 0, j, xLen, yLen)
+		depthFirstSearch(board, xLen - 1, j, xLen, yLen)
+	}
+	for (let m = 0; m < xLen; m++) {
+		for (let n = 0; n < yLen; n++) {
+			if (board[m][n] === '-')
+				board[m][n] = 'O'
+			else if (board[m][n] === 'O')
+				board[m][n] = 'X'
+		}
+	}
 }
 
 const depthFirstSearch = (board, x, y, xLen, yLen) => {
