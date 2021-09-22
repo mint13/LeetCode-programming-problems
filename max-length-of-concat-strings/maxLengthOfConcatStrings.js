9f8104696dba87bee718571305a242650becd525
Mint Medillo <m.medillo13@gmail.com>
2021-09-22 18:56:48 +0800

    add logic for maxLength

--- max-length-of-concat-strings/maxLengthOfConcatStrings.js
+++ max-length-of-concat-strings/maxLengthOfConcatStrings.js
@@ -3,7 +3,22 @@
  * @return {number}
  */
 const maxLength = function (arr) {
-
+  let max = 0
+ 
+  const aux = (index, current = '') => {
+    if (index > arr.length - 1) {
+      max = Math.max(current.length, max)
+      return
+    }
+    if (isUnique(current)) {
+      aux(index + 1, current)
+      if (isUnique(current + arr[index])) {
+        aux(index + 1, current + arr[index])
+      }
+    }
+  }
+  aux(0)
+  return max
 }
 
 const isUnique = (s = '') => {
