e6d2c071c29aea1fff9502367ed13c11aca0767a
Mint Medillo <m.medillo13@gmail.com>
2021-09-22 18:55:59 +0800

    add check function isUnique

--- max-length-of-concat-strings/maxLengthOfConcatStrings.js
+++ max-length-of-concat-strings/maxLengthOfConcatStrings.js
@@ -2,3 +2,17 @@
  * @param {string[]} arr
  * @return {number}
  */
+const maxLength = function (arr) {
+
+}
+
+const isUnique = (s = '') => {
+	const map = {}
+	for (const c of s) {
+		map[c] = (map[c] || 0) + 1
+		if (map[c] >= 2) {
+			return false
+		}
+	}
+	return true
+}
