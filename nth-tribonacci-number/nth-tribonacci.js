21b1f6953727e2d5164bb2d75778483d5dfbaf11
Mint Medillo <m.medillo13@gmail.com>
2021-09-24 21:33:45 +0800

    add tribonacci function logic

--- nth-tribonacci-number/nth-tribonacci.js
+++ nth-tribonacci-number/nth-tribonacci.js
@@ -4,5 +4,17 @@
  */
 
 const tribonacci = (n) => { 
-
+	// You first want to state the begining 
+	let a = 0, b = 1, c = 1
+    
+	// Repeat for n times 
+	for (let i = 0; i < n; i++) { 
+		// Set temporary variable to use later 
+		let temp = c 
+		// set a, b, and c to next items in sequence a -> b b -> c c-> a + b + c 
+		c = a + b + c 
+		a = b 
+		b = temp
+	} 
+	return a // return the end
 }
