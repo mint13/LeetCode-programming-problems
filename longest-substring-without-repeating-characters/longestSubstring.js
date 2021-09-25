9f15ee9f47a831923efe011f8117c67657a0e643
Mint Medillo <m.medillo13@gmail.com>
2021-09-25 14:24:55 +0800

    add logic handling loop

--- longest-substring-without-repeating-characters/longestSubstring.js
+++ longest-substring-without-repeating-characters/longestSubstring.js
@@ -20,5 +20,19 @@ const lengthOfLongestSubstring = (s) => {
 	// convert our strings to an array
 	const strings = s.split('')
 
+	//iterate over the array
+	for (let i = 0; i < length; i++) {
+		//if the character exist and the value of the character is greater or equal to our start index
+		if (hashTable[strings[i]] !== undefined && hashTable[strings[i]] >= start) {
+			//change the value of start to one higher than the value of our current character 
+			start = hashTable[strings[i]] + 1
+		}
+
+		//add the current index and it's value to the hashTable
+		hashTable[strings[i]] = i
+		//find the length of the longest substring by comparing the value with the value of the current index minus the start value plus 1
+		longestSubstringLength = Math.max(longestSubstringLength, (i - start + 1))
+	}
+
 	return longestSubstringLength
 }
