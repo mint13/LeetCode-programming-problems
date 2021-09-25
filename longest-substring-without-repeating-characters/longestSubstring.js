ab22a2da12d2dec0df1368260de29f89f297ee6b
Mint Medillo <m.medillo13@gmail.com>
2021-09-25 14:24:13 +0800

    define variables needed

--- longest-substring-without-repeating-characters/longestSubstring.js
+++ longest-substring-without-repeating-characters/longestSubstring.js
@@ -4,5 +4,21 @@
  */
 
 const lengthOfLongestSubstring = (s) => {
+	// if string input is empty or not a string, return 0
+	if (!!!s.length || typeof s !== 'string') 
+		return 0
+	
+	// if the length is 1, return 1
+	if (s.length  == 1)
+		return 1
 
+	let hashTable = {} // hashTable to hold the characters and index;
+	let longestSubstringLength = 0 // length of longest substring
+	let start = 0 // start index
+	const length = s.length // length of the array.
+
+	// convert our strings to an array
+	const strings = s.split('')
+
+	return longestSubstringLength
 }
