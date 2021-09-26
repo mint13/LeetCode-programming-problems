2aa3a9d65bfb0ab2807bd50b0c01eb0f325abe79
Mint Medillo <m.medillo13@gmail.com>
2021-09-26 22:38:40 +0800

    add subarraysWithKDistinct function logic

--- subarrays-with-k-different-integers/subarraysWithKDifferentInteger.js
+++ subarrays-with-k-different-integers/subarraysWithKDifferentInteger.js
@@ -4,4 +4,29 @@
  * @return {number}
  */
 const subarraysWithKDistinct = (A, K) => {
+	const atMost = (count) => {
+		const map = {}
+		let left = 0
+		let distinct = 0
+		let result = 0
+		for (let right = 0; right < A.length; right++) {
+			const num = A[right]
+			map[num] = (map[num] || 0) + 1
+
+			if (map[num] === 1) {
+				distinct += 1
+			}
+			while (distinct > count) {
+				map[A[left]] -= 1
+				if (map[A[left]] === 0) {
+					distinct -= 1
+				}
+
+				left += 1
+			}
+			result += right - left + 1
+		}
+		return result
+	}
+	return atMost(K) - atMost(K - 1)
 }
