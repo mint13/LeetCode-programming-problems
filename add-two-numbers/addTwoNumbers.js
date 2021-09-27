f35ad728a4266045d4a1bc5773698cbf9fd72dc6
Mint Medillo <m.medillo13@gmail.com>
2021-09-27 13:09:22 +0800

    add addTwoNumbers function logic

--- add-two-numbers/addTwoNumbers.js
+++ add-two-numbers/addTwoNumbers.js
@@ -11,6 +11,39 @@
  * @return {ListNode}
  */
 const addTwoNumbers = (l1, l2) => {
+	let h1 = l1
+	let h2 = l2
+	let digit = 0
+	let head = null
+	let current = null
+
+	while (h1 || h2) {
+		const num1 = h1 ? h1.val : 0
+		const num2 = h2 ? h2.val : 0
+		let value = num1 + num2 + digit
+		
+		digit = 0
+
+		if (value >= 10) {
+			value -= 10
+			digit = 1
+		}
+
+		h1 = h1 ? h1.next : null
+		h2 = h2 ? h2.next : null
+		const node = new ListNode(value)
+		if (!head) {
+			head = node
+			current = node
+		} else {
+			current.next = node
+			current = node
+		}
+	}
+	if (digit === 1) {
+		current.next = new ListNode(1)
+	}
+	return head
 }
 
 function ListNode(val) {
