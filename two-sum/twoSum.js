850ad4d56af46214ef420e0fb043b25b5e422edc
Mint Medillo <m.medillo13@gmail.com>
2021-09-28 22:23:13 +0800

    add twoSum function logic

--- two-sum/twoSum.js
+++ two-sum/twoSum.js
@@ -4,5 +4,13 @@
  * @return {number[]}
  */
  const twoSum = (nums, target) => {
-    
+	const memo = {}
+	const aux = (index) => {
+		if (memo[nums[index]] !== undefined) {
+			return [memo[nums[index]], index]
+		}
+		memo[target - nums[index]] = index
+		return aux(index + 1)
+	}
+	return aux(0, nums.length - 1)
 }
