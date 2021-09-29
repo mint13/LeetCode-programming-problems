951bc931880a17bf633a51b0974d2414f84d24c6
Mint Medillo <m.medillo13@gmail.com>
2021-09-29 19:21:05 +0800

    create merge function

--- median-of-two-sorted-arrays/medianOfTwoSortedArrays.js
+++ median-of-two-sorted-arrays/medianOfTwoSortedArrays.js
@@ -6,3 +6,15 @@
  const findMedianSortedArrays = (nums1, nums2) => {
     
 }
+ 
+const merge = (xs1, xs2) => {
+	if (!xs1 || !xs1.length) {
+		return xs2
+	}
+	if (!xs2 || !xs2.length) {
+		return xs1
+	}
+	const [hd1, ...rest1] = xs1
+	const [hd2, ...rest2] = xs2
+	return hd1 <= hd2 ? [hd1, ...merge(rest1, xs2)] : [hd2, ...merge(xs1, rest2)]
+}
