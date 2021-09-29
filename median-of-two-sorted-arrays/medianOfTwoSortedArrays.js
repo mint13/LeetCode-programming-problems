5e016408b68588e92a144de3864b5d19a5031196
Mint Medillo <m.medillo13@gmail.com>
2021-09-29 19:21:42 +0800

    add findMedianSortedArrays function logic

--- median-of-two-sorted-arrays/medianOfTwoSortedArrays.js
+++ median-of-two-sorted-arrays/medianOfTwoSortedArrays.js
@@ -3,10 +3,13 @@
  * @param {number[]} nums2
  * @return {number}
  */
- const findMedianSortedArrays = (nums1, nums2) => {
-    
+const findMedianSortedArrays = (nums1, nums2) => {
+	const nums = merge(nums1, nums2)
+	const middle = Math.floor((nums.length-1) / 2)
+
+	return (middle * 2 === (nums.length-1)) ?  nums[middle] : ((nums[middle] + nums[middle + 1]) / 2) 
 }
- 
+
 const merge = (xs1, xs2) => {
 	if (!xs1 || !xs1.length) {
 		return xs2
