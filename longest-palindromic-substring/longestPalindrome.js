3151ff7d943a99195872734b85c2a7c5b1c5b5f2
Mint Medillo <m.medillo13@gmail.com>
2021-09-30 22:26:32 +0800

    add expandAroundCenter function

--- longest-palindromic-substring/longestPalindrome.js
+++ longest-palindromic-substring/longestPalindrome.js
@@ -6,3 +6,14 @@
 const longestPalindrome = (s) => {
 
 }
+
+const expandAroundCenter = (s, leftIdx, rightIdx) => {
+	while (leftIdx >= 0 && rightIdx < s.length) {
+		if (s[leftIdx] !== s[rightIdx])
+			break
+		
+		leftIdx--
+		rightIdx++
+	}
+	return [leftIdx + 1, rightIdx] 
+}
