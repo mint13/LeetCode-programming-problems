79051bcd2a1aa8d8b2701ebcb2487f686bbd34e1
Mint Medillo <m.medillo13@gmail.com>
2021-10-01 17:19:42 +0800

    add convert function logic

--- zigzag-conversion/zigzagConversion.js
+++ zigzag-conversion/zigzagConversion.js
@@ -3,6 +3,27 @@
  * @param {number} numRows
  * @return {string}
  */
-const convert = (s, numRows) => {
+ const convert = (s, numRows) => {
+	if (numRows === 1) {
+		return s
+	}
+	const arr = new Array(numRows).fill("")
+	let i = 0
+	let down = true
+	for (const c of s) {
+		arr[i] += c
+		if (down) {
+			i += 1
+			if (i === numRows - 1) {
+				down = false
+			}
+			continue
+		}
 
+		i -= 1
+		if (i === 0) {
+			down = true
+		}
+	}
+	return arr.join('')
 }
