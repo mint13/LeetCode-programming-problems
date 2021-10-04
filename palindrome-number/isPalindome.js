b98b406d62b54c19fd84cb58cec2e20ce093ca55
Mint Medillo <m.medillo13@gmail.com>
2021-10-04 19:48:22 +0800

    add isPalindrome function  logic

--- palindrome-number/isPalindome.js
+++ palindrome-number/isPalindome.js
@@ -3,5 +3,15 @@
  * @return {boolean}
  */
 const isPalindrome = function (x) {
+	if (x < 0)
+		return false
 
+	let reverse = 0, num = x
+
+	while (num > 0) {
+		reverse = reverse * 10 + (num % 10)
+		num = Math.floor(num / 10)
+	}
+	
+	return reverse === x
 }
