70a20aa21d3429e53bb1af21a25f77a326617555
Mint Medillo <m.medillo13@gmail.com>
2021-10-06 20:00:26 +0800

    add maxArea function logic

--- container-with-most-water/containerWithMostWater.js
+++ container-with-most-water/containerWithMostWater.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 const maxArea = (height = []) => {
+	let max = 0
+	let left = 0
+	let right = height.length - 1
 
+	while (left < right) {
+		max = Math.max(max, Math.min(height[left], height[right]) * (right - left))
+
+		if (height[left] <= height[right]) {
+			left += 1
+			continue
+		}
+
+		right -= 1
+	}
+	
+	return max
 }
