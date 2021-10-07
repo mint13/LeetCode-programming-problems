f513aadc80418d3658ce3a4166588070db90c7d1
Mint Medillo <m.medillo13@gmail.com>
2021-10-07 17:38:08 +0800

    add longestCommonPrefix function logic

--- longest-common-prefix/longestCommonPrefix.js
+++ longest-common-prefix/longestCommonPrefix.js
@@ -3,4 +3,19 @@
  * @return {string}
  */
 const longestCommonPrefix = (strs) => {
+	if (strs.length === 0)
+		return ''
+	
+	const len1 = strs.length, len2 = strs[0].length
+	let tmp = '', res = ''
+
+	outer: for (var i = 0; i < len2; i++) {
+		tmp = strs[0][i];
+		for (var j = 1; j < len1; j++) {
+			if (strs[j][i] !== tmp)
+				break outer
+		}
+		res += tmp
+	}
+	return res
 }
