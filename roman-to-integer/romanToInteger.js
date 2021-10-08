d04e9f600cd2b7335d187e7bfd63a4789c36c367
Mint Medillo <m.medillo13@gmail.com>
2021-10-08 19:05:24 +0800

    add romanToInt function logic

--- roman-to-integer/romanToInteger.js
+++ roman-to-integer/romanToInteger.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 const romanToInt = (s) => {
-
+	const table = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000
+	}
+	
+	let result = 0
+	for (let i = 0; i < s.length; i++) {
+		if (table[s[i]] < table[s[i + 1]]) { // if the next roman numeral is larger, then we know we have to subtract this number
+			result -= table[s[i]]
+			continue
+		} 
+		result += table[s[i]]
+	}
+	return result
 }
