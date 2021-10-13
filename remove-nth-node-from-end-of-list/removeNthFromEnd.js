a96d370d967285a40477959e29bea954aa8a87ee
Mint Medillo <m.medillo13@gmail.com>
2021-10-13 18:10:35 +0800

    add removeNthFromEnd function logic

--- remove-nth-node-from-end-of-list/removeNthFromEnd.js
+++ remove-nth-node-from-end-of-list/removeNthFromEnd.js
@@ -4,5 +4,25 @@
  * @return {ListNode}
  */
 const removeNthFromEnd = (head, n) => {
+	let h1 = head
+	let h2 = null
+	let count = 0
 
+	while (h1) {
+		count += 1
+		h1 = h1.next
+		if (h2) {
+			h2 = h2.next
+		}
+		if (count === n + 1) {
+			h2 = head
+		}
+	}
+
+	if (!h2) {
+		return head ? head.next : null
+	}
+	
+	h2.next = h2.next.next
+	return head
 }
