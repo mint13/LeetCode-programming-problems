07f8959bf30e2d0b7872c9bd4fcffbdddfc19c8b
Mint Medillo <m.medillo13@gmail.com>
2021-10-14 19:40:06 +0800

    add valid parentheses problem

---
+++ valid-parentheses/validParentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = (s) => {
+	const stack = []
+	const pair = {
+		'[': ']',
+		'{': '}',
+		'(': ')',
+	}
+	const isPaired = (a, b) => pair[a] === b
+
+	for (c of s) {
+		if (isPaired(stack[stack.length - 1], c)) {
+			stack.pop()
+			continue
+		}
+			stack.push(c)
+	}
+	return stack.length === 0
+}
