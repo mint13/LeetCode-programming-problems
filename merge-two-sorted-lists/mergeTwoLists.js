f6d74062e3cfabf2be3c48917024e3aff6a22d86
Mint Medillo <m.medillo13@gmail.com>
2021-10-15 17:54:10 +0800

    add mergeTwoLists function logic

--- merge-two-sorted-lists/mergeTwoLists.js
+++ merge-two-sorted-lists/mergeTwoLists.js
@@ -11,7 +11,10 @@
  * @return {ListNode}
  */
 const mergeTwoLists = (l1, l2) => {
-
+	if (!l1 || !l2)
+		return l1 || l2
+	
+	return l1.val < l2.val ? linkThem(l1,l2) : linkThem(l2,l1)
 }
 
 const linkThem = (smaller, greater) => {
