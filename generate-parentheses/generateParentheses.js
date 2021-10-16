e173fcadd3b4a9b96d1d631758e826004c53fa28
Mint Medillo <m.medillo13@gmail.com>
2021-10-16 18:32:12 +0800

    add generateParentheses function logic

--- generate-parentheses/generateParentheses.js
+++ generate-parentheses/generateParentheses.js
@@ -3,5 +3,18 @@
  * @return {string[]}
  */
 const generateParentheses = (n) => {
-
+  const result = []
+  const aux = (current, left, right) => {
+    if (left === n && right === n) {
+      result.push(current)
+      return
+    }
+    if (left < right || left > n || right > n) {
+      return
+    }
+    aux(`${current}(`, left + 1, right)
+    aux(`${current})`, left, right + 1)
+  }
+  aux('', 0, 0)
+  return result
 }
