298f2fc1775032468536a1f00abe27f3defa2397
Mint Medillo <m.medillo13@gmail.com>
2021-10-18 14:57:48 +0800

    add swapPairs function logic

--- swap-node-in-pairs/swapNodePairs.js
+++ swap-node-in-pairs/swapNodePairs.js
@@ -3,7 +3,19 @@
  * @return {ListNode}
  */
 const swapPairs = (head) => {
+	const newHead = new ListNode(0)
+	newHead.next = head
+	let current = newHead
 
+	while (current && current.next && current.next.next) {
+    const temp = current.next
+    current.next = current.next.next
+    const { next } = current.next
+    current.next.next = temp
+    temp.next = next
+    current = current.next.next
+	}
+	return newHead.next
 }
 
 /**
