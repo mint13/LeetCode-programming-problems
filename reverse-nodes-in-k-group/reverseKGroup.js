86cd8e420e5fc53c48e0018fe85dfaaa87f11e9a
Mint Medillo <m.medillo13@gmail.com>
2021-10-21 21:27:05 +0800

    add getLength function to identify current node length

--- reverse-nodes-in-k-group/reverseKGroup.js
+++ reverse-nodes-in-k-group/reverseKGroup.js
@@ -6,3 +6,13 @@
  const reverseKGroup = (head, k) => {
 
 }
+
+const getLength = (node) => {
+	let length = 0
+	let current = node
+	while (current) {
+		length += 1
+		current = current.next
+	}
+	return length
+}
