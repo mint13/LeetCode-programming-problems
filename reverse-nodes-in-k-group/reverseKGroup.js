95f331fc326236f104c50f809a3128e076472b3c
Mint Medillo <m.medillo13@gmail.com>
2021-10-21 21:29:08 +0800

    add reverse function

--- reverse-nodes-in-k-group/reverseKGroup.js
+++ reverse-nodes-in-k-group/reverseKGroup.js
@@ -7,6 +7,43 @@
 
 }
 
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+ const reverseKGroup = (head, k) => {
+
+}
+
+const reverse = (node, k) => {
+	let count = 1
+	let current = node
+	let next = null
+	let newHead = null
+
+	while (count <= k && current) {
+		newHead = current
+		const temp = current.next
+		current.next = next
+		next = current
+		current = temp
+		count += 1
+	}
+	node.next = current
+	return [newHead, node]
+}
+
+const getLength = (node) => {
+	let length = 0
+	let current = node
+	while (current) {
+		length += 1
+		current = current.next
+	}
+	return length
+} 
+
 const getLength = (node) => {
 	let length = 0
 	let current = node
