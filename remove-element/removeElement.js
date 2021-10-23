ea62063f259bc89dc7f01b052191b15863553f15
Mint Medillo <m.medillo13@gmail.com>
2021-10-23 21:18:36 +0800

    add removeElement function logid

--- remove-element/removeElement.js
+++ remove-element/removeElement.js
@@ -10,4 +10,19 @@
 		nums[a] = nums[b]
 		nums[b] = temp
 	}
+	while (left <= nums.length - 1) {
+		while (nums[nums.length - 1] === val) {
+			nums.pop()
+		}
+		while (nums[left] !== val && left <= nums.length - 1) {
+			left += 1
+		}
+
+		if (left < nums.length - 1) {
+			swap(left, nums.length - 1)
+			left += 1
+			nums.pop()
+		}
+	}
+	return nums.length
 }
