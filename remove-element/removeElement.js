f9ee8f9c24bd917a6d9ca481aec28f20897f5151
Mint Medillo <m.medillo13@gmail.com>
2021-10-23 21:18:04 +0800

    add swap utility function

--- remove-element/removeElement.js
+++ remove-element/removeElement.js
@@ -4,5 +4,10 @@
  * @return {number}
  */
  const removeElement = function (nums, val) {
-
+	let left = 0
+	const swap = (a, b) => {
+		const temp = nums[a]
+		nums[a] = nums[b]
+		nums[b] = temp
+	}
 }
