dd114864eb2b4d730047680c19e58795cbec94fd
Mint Medillo <m.medillo13@gmail.com>
2021-10-23 21:12:00 +0800

    add removeDuplicates function logic

--- remove-duplicates-from-sorted-array/removeDuplicates.js
+++ remove-duplicates-from-sorted-array/removeDuplicates.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
  const removeDuplicates = (nums) => {
-
+	let left = 0
+	for (let right = 1; right < nums.length; right++) {
+		if (nums[right] !== nums[left]) {
+			left += 1
+			nums[left] = nums[right]
+		}
+	}
+	return left + 1
 }
