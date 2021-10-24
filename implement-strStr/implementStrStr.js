f0f15f0bb83b5abae4eb8aba28e8852c251e4743
Mint Medillo <m.medillo13@gmail.com>
2021-10-24 22:32:21 +0800

    add strStr function logic

--- implement-strStr/implementStrStr.js
+++ implement-strStr/implementStrStr.js
@@ -4,5 +4,17 @@
  * @return {number}
  */
 const strStr = (haystack, needle) => {
+	if (needle.length === 0)
+		return 0
 
+	if (needle === haystack)
+		return 0
+
+	for (let i = 0; i <= haystack.length - needle.length; i++) {
+		if (needle === haystack.substring(i, i + needle.length)) {
+			return i
+		}
+	}
+
+	return -1
 }
