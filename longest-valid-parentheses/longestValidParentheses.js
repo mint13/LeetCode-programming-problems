dee742d88cc49b78951656c8ab4d2186d05b43a3
Mint Medillo <m.medillo13@gmail.com>
2021-10-27 21:36:40 +0800

    add longestValidParentheses function logic

--- longest-valid-parentheses/longestValidParentheses.js
+++ longest-valid-parentheses/longestValidParentheses.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
  const longestValidParentheses = (s = '') => {
+	const stack = [-1]
+	let max = 0
+	for (let i = 0; i < s.length; i++) {
+		if (s[stack[stack.length - 1]] === '(' && s[i] === ')') {
+			stack.pop()
+		} else {
+			max = Math.max(max, i - stack[stack.length - 1] - 1)
+			stack.push(i)
+		}
+	}
 
+	return Math.max(max, s.length - stack[stack.length - 1] - 1)
 }
