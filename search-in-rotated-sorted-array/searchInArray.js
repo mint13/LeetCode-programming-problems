d274dd3a011a511a7e5190066a3f620dabd26465
Mint Medillo <m.medillo13@gmail.com>
2021-10-28 20:16:02 +0800

    add search function logic

--- search-in-rotated-sorted-array/searchInArray.js
+++ search-in-rotated-sorted-array/searchInArray.js
@@ -4,5 +4,31 @@
  * @return {number}
  */
 const search = (nums, target) => {
+	const len = nums.length
+	let left = 0,
+		right = len - 1,
+		mid = 0
 
+	while (left <= right) {
+		mid = left + Math.floor((right - left) / 2)
+
+		if (nums[mid] === target)
+			return
+			
+		if (nums[mid] > nums[right]) {
+			if (nums[left] <= target && target < nums[mid]) {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if (nums[mid] < target && target <= nums[right]) {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
 }
