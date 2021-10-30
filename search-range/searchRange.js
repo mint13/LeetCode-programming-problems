4b1b830b2ba74dc66b7b0c69427fd178bec883f4
Mint Medillo <m.medillo13@gmail.com>
2021-10-30 20:35:18 +0800

    add find utility function

--- search-range/searchRange.js
+++ search-range/searchRange.js
@@ -6,3 +6,20 @@
 const searchRange = (nums, target) => {
 
 }
+
+const find = (nums, target, findLeft) => {
+	let left = 0,
+		right = nums.length - 1,
+		mid = 0
+
+	while (left <= right) {
+		mid = Math.floor((left + right) / 2)
+		if (nums[mid] > target || (findLeft && nums[mid] === target)) {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return findLeft ? left : right
+}
