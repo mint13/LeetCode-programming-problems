5b5c1804757401068e1934bc4fa6bfd5499007c0
Mint Medillo <m.medillo13@gmail.com>
2021-11-01 14:55:28 +0800

    add searchInsert function logic

--- search-insert-position/searchPosition.js
+++ search-insert-position/searchPosition.js
@@ -4,5 +4,20 @@
  * @return {number}
  */
 const searchInsert = (nums, target) => {
-
+	const aux = (low, high) => {
+		if (low > high) {
+			return low
+		}
+		const middle = Math.floor((low + high) / 2)
+		
+		if (target === nums[middle]) {
+			return middle
+		}
+		
+		if (target > nums[middle]) {
+			return aux(middle + 1, high)
+		}
+		return aux(low, middle - 1)
+	}
+	return aux(0, nums.length - 1)
 }
