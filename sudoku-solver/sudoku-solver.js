a49a1efe43f37eeaf8f47c7293b3d149bbac3c2e
Mint Medillo <m.medillo13@gmail.com>
2021-11-09 23:22:09 +0800

    add isValid utility function

--- sudoku-solver/sudoku-solver.js
+++ sudoku-solver/sudoku-solver.js
@@ -4,3 +4,17 @@
  */
 const solveSudoku = (board) => {
 }
+  
+const isValid = (board, i, j, num) => {
+	for (let k = 0; k < 9; k++) {
+		if (board[i][k] === num)
+			return false
+
+		if (board[k][j] === num)
+			return false
+
+		if (board[Math.floor(i / 3) * 3 + Math.floor(k / 3)][Math.floor(j / 3) * 3 + (k % 3)] === num)
+			return false
+	}
+	return true
+}
