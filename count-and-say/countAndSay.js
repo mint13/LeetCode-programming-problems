e3b30eda69ac105460a7c5f7745b9c473968ac0b
Mint Medillo <m.medillo13@gmail.com>
2021-11-10 20:46:11 +0800

    add countAndSay function logic

--- count-and-say/countAndSay.js
+++ count-and-say/countAndSay.js
@@ -3,5 +3,39 @@
  * @return {string}
  */
 const countAndSay = function(n) {
+	const len = 0
+	let str = '1',
+		tmp = '',
+		last = '',
+		count = 0
 
+	for (let i = 1; i < n; i++) {
+		tmp = ''
+		last = ''
+		count = 0
+		len = str.length
+
+		for (let j = 0; j < len; j++) {
+			if (last === '') {
+				last = str[j]
+				count = 1
+				continue
+			}
+			if (str[j] === last) {
+				count += 1
+			} else {
+				tmp += '' + count + last
+				last = str[j]
+				count = 1
+			}
+		}
+
+		if (last) {
+			tmp += '' + count + last
+		}
+
+		str = tmp
+	}
+
+	return str
 }
