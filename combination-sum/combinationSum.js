ea31491486c40374ea52bfeb0e59a16ddd3f5167
Mint Medillo <m.medillo13@gmail.com>
2021-11-12 22:12:26 +0800

    add combinationSum function logic

--- combination-sum/combinationSum.js
+++ combination-sum/combinationSum.js
@@ -4,5 +4,24 @@
  * @return {number[][]}
  */
 const combinationSum = (candidates, target) => {
+	const result = []
+	const aux = (start, current=[], sum = 0) => {
+			if (sum > target) {
+					return 
+			}
+			
+			if (sum === target) {
+					result.push(current)
+					return
+			}
 
+			for (let i = start; i < candidates.length; i++) {
+				aux(i, [...current, candidates[i]], sum + candidates[i])
+			}
+	}
+
+	for (let i = 0; i <  candidates.length; i++) {
+		aux(i, [candidates[i]], candidates[i])
+	}
+	return result
 }
